d33dd1a5ff6d7270847791eebd0d65672d3d7acf
Meghanaalla <41770175+Meghanaalla@users.noreply.github.com>
2018-08-09 15:17:45 +0530

    Update natural.py

--- natural.py
+++ natural.py
@@ -1,7 +1,7 @@
-n=int(raw_input())
+p=int(raw_input())
 sum=0
-if(n>0):
+if(p>0):
        while(n>0):
-                 sum+=n
-                 n-=1
+                 sum+=p
+                 p-=1
        print(sum)
